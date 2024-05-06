ff8a9b7721a4da9f3e0f50637ccbc95957a1c11f
Singrid Maria <singrid.maria@gmail.com>
2024-05-06 19:31:03 -0300

    aula 06/05/2024

--- app/view/reso_products.py
+++ app/view/reso_products.py
@@ -5,6 +5,16 @@ from flask_restful import Resource, reqparse
 argumentos = reqparse.RequestParser()
 argumentos.add_argument('name', type=str)
 argumentos.add_argument('price', type=float)
+
+#atualizar
+argumentos_update = reqparse.RequestParses()
+argumentos_update.add_argument('id', type=int)
+argumentos_update.add_argument('name', type=str)
+argumentos_update.add_argument('price', type=float)
+
+
+
+
 class Index(Resource):
     def get(self):
         return jsonify("Welcome Aplication Flask")
@@ -16,5 +26,14 @@ class ProductsCreate(Resource):
             print(datas)
             Products.save_products(self,datas['name'], datas['price'])
             return {"message": 'Products create sucessfully'},200
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
+        
+class ProductUpdate(Resource):
+    def put(self):
+        try:
+            datas = argumentos_update.parse_args()
+            Products.update_products(self,datas['id'],datas['name'],datas['price'])
+            return {"message": 'Products update sucessfully'},200
         except Exception as e:
             return jsonify({'status': 500, 'msg': f'{e}'}), 500
