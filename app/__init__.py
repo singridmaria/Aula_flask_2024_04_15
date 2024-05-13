68ba26b3ca28b2bdc7ecac58128451fe59fc2f12
Singrid Maria <singrid.maria@gmail.com>
2024-05-13 20:54:29 -0300

    Aula 13052024

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+
 app = Flask(__name__)
+api= Api(app)
+#Configuração com banco de dados
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app) 
-api= Api(app)
 from app.models.products import Products
 with app.app_context():
     db.create_all()
 
-from app.view.reso_products import Index, ProductsCreate
-api.add_resource(Index,'/')
+from app.view.reso_products import Index, ProductsCreate, ProductUpdate, ProductDelete
+api.add_resource(Index,'/') #Como se fosse a rota, so que com a chamada da api
 api.add_resource(ProductsCreate,'/criar')
+api.add_resource(ProductUpdate,'/atualizar')
+api.add_resource(ProductDelete,'/deletar')
 
 
 '''@app.route("/")
--- app/view/reso_products.py
+++ app/view/reso_products.py
@@ -7,11 +7,14 @@ argumentos.add_argument('name', type=str)
 argumentos.add_argument('price', type=float)
 
 #atualizar
-argumentos_update = reqparse.RequestParses()
+argumentos_update = reqparse.RequestParser()
 argumentos_update.add_argument('id', type=int)
 argumentos_update.add_argument('name', type=str)
 argumentos_update.add_argument('price', type=float)
 
+#deletar
+argumentos_deletar =  reqparse.RequestParser()
+argumentos_deletar.add_argument('id', type=int)
 
 
 
@@ -35,5 +38,15 @@ class ProductUpdate(Resource):
             datas = argumentos_update.parse_args()
             Products.update_products(self,datas['id'],datas['name'],datas['price'])
             return {"message": 'Products update sucessfully'},200
+        except Exception as e:
+            return jsonify({'status': 500, 'msg': f'{e}'}), 500
+
+
+class ProductDelete(Resource):
+    def delete(self):
+        try:
+            datas = argumentos_deletar.parse_args()
+            Products.delete_products(self,datas['id'])
+            return {"message": 'Products delete sucessfully'},200
         except Exception as e:
             return jsonify({'status': 500, 'msg': f'{e}'}), 500
